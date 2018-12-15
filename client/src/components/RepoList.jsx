e4732aa78444000662dc02ec9a6f7443eed14e25
Rudy <rudydimacali@gmail.com>
2018-12-15 10:24:01 -0800

    COMPLETE BASIC REQUIREMENTS.

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const RepoList = props => (
   <div>
     <h4> Repo List Component </h4>
-    There are {props.count} repos. Currently displaying the top{" "}
+    There are {props.count} repos in the database. Currently displaying the top{" "}
     {props.repos.length} repos.
   </div>
 );
