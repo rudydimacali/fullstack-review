6efe36ff7d4b7440c90da91e363d2820b520b9fe
Rudy <rudydimacali@gmail.com>
2018-12-14 16:52:24 -0800

    Add table rendering for repos.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,6 +10,7 @@ class App extends React.Component {
     this.state = {
       repos: []
     };
+    this.RepoTableDisplay = this.RepoTableDisplay.bind(this);
   }
 
   search(term) {
@@ -35,13 +36,50 @@ class App extends React.Component {
       }
     });
   }
-
+  RepoTableDisplay() {
+    let repos = this.state.repos;
+    let reposDisplay = repos.map(repo => {
+      return (
+        <tr>
+          <td>
+            {repo.owner.login}
+            <img src={repo.owner.avatar_url} />
+          </td>
+          <td>{repo.owner.html_url}</td>
+          <td>{repo.name}</td>
+          <td>{repo.html_url}</td>
+          <td>{repo.created_at}</td>
+          <td>{repo.updated_at}</td>
+          <td>{repo.stargazers_count}</td>
+          <td>{repo.watchers_count}</td>
+          <td>{repo.forks_count}</td>
+        </tr>
+      );
+    });
+    return (
+      <table>
+        <tr>
+          <th>Username</th>
+          <th>User URL</th>
+          <th>Repo Name</th>
+          <th>Repo Link</th>
+          <th>Created At</th>
+          <th>Last Updated</th>
+          <th>Stars</th>
+          <th>Watchers</th>
+          <th>Forks</th>
+        </tr>
+        {reposDisplay}
+      </table>
+    );
+  }
   render() {
     return (
       <div>
         <h1>Github Fetcher</h1>
         <RepoList repos={this.state.repos} />
         <Search onSearch={this.search.bind(this)} />
+        <this.RepoTableDisplay />
       </div>
     );
   }
