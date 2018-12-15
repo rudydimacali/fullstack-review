07adfa921ef4c0a63e3a6a57b27b0ebfc2e50f5a
Rudy <rudydimacali@gmail.com>
2018-12-14 19:08:33 -0800

    Fix bug for searching for nonexisting users.

--- database/index.js
+++ database/index.js
@@ -31,24 +31,30 @@ let save = (repoArray, callback) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
-  repoArray.forEach(repo => {
-    Repo.find({ id: repo.id }, (err, repoList) => {
-      if (err) {
-        throw err;
-      } else if (repoList.length === 0) {
-        Repo.create(repo, err => {
-          if (err) {
-            throw err;
-          } else {
-            console.log("Success!");
-          }
-        });
-      } else {
-        console.log("Repo exists in database!");
-      }
+  console.log("TESTING SAVE", repoArray);
+  console.log(repoArray.message);
+  if (!repoArray.message) {
+    repoArray.forEach(repo => {
+      Repo.find({ id: repo.id }, (err, repoList) => {
+        if (err) {
+          throw err;
+        } else if (repoList.length === 0) {
+          Repo.create(repo, err => {
+            if (err) {
+              throw err;
+            } else {
+              console.log("Success!");
+            }
+          });
+        } else {
+          console.log("Repo exists in database!");
+        }
+      });
     });
-  });
-  callback();
+    callback(null);
+  } else {
+    callback();
+  }
 };
 
 let getRepos = callback => {
--- server/index.js
+++ server/index.js
@@ -8,19 +8,21 @@ app.use(express.static(__dirname + "/../client/dist"));
 app.use(bodyParser());
 
 app.post("/repos", function(req, res) {
-  // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
-  githubHelper.getReposByUsername(req.body.username, response => {
-    repoDatabase.save(response, () => {
-      res.send(req.body.username);
-    });
+  githubHelper.getReposByUsername(req.body.username, (err, response) => {
+    if (err) {
+      res.send("User does not exist!");
+    } else {
+      repoDatabase.save(response, () => {
+        res.send(req.body.username);
+      });
+    }
   });
 });
 
 app.get("/repos", function(req, res) {
-  // TODO - your code here!
   // This route should send back the top 25 repos
   repoDatabase.getRepos((err, response) => {
     if (err) {
